c899c2dff7dbf9bd0f59c32a5f3b6de90e94d29a
Akir Oussama <akir.oussama@gmail.com>
2023-05-03 13:36:36 +0200

    Type Annotations With Array

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,10 @@
 "use strict";
-let theName = "Wild";
-let theAge = 40;
-let hire = true;
-let all = "Wild Code School";
-let allVars;
-theName = "Fawsy";
+let all = "Baptiste";
+all = "A";
 all = 100;
-function add(n1, n2) {
-    return n1 + n2;
+all = true;
+let myFriends = ["Nathan", "Greg", "Ludivine"];
+for (let i = 0; i < myFriends.length; i++) {
+    console.log(myFriends[i]);
 }
-console.log(add(10, 20));
-console.log(typeof add(10, 20));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,20 @@
 /*
-Annotations de type || Signature
-  -- Indiquez le type de données des variables
-  -- Indiquer le type de données des fonctions d'entrée/sortie
-  -- Objets, etc...
-
-  - Pourquoi l'utilisons-nous ?
-  - Est-ce obligatoire ?
-  - Que se passe-t-il si nous ne l'utilisons pas ?
+  Type Annotations With Arrays
 */
 
-let theName = "Wild";
-let theAge: number = 40;
-let hire: boolean = true;
-let all: any = "Wild Code School";
-let allVars; // Any
+let all: string | number | boolean = "Baptiste";
 
-theName = "Fawsy";
+all = "A";
 all = 100;
+all = true;
+// all = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-function add(n1: number, n2: number) {
-  return n1 + n2;
+// Comment déduire le type d'un tableau ?
+let myFriends: string[] = ["Nathan", "Greg", "Ludivine"];
+
+//let myFriends = ["Nathan", "Greg", "Ludivine", 10];
+
+for (let i = 0; i < myFriends.length; i++) {
+  console.log(myFriends[i]);
+  //console.log(myFriends[i].repeat(3));
 }
-
-console.log(add(10, 20));
-console.log(typeof add(10, 20));
-
-// function add(n1, n2) {
-//   return n1 + n2;
-// }
-
-// console.log(add(10, "20"));
-// console.log(typeof add(10, "20"));
