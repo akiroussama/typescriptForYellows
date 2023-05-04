27149e1b179d005329eb67cd85d876e2a4f480e4
Akir Oussama <akir.oussama@gmail.com>
2023-05-04 06:43:45 +0200

    advanced alias

--- src/index.ts
+++ src/index.ts
@@ -12,3 +12,29 @@ let all: standnum = 10;
 all = 100;
 all = "Baptiste";
 all = true;
+
+/*
+  Data Types
+  - Advanced Type Alias
+*/
+
+type Buttons = {
+  up: string;
+  right: string;
+  down: string;
+  left: string;
+};
+
+function getActions(btns: Buttons) {
+  console.log(`Action For Button Up Is ${btns.up}`);
+  console.log(`Action For Button Right Is ${btns.right}`);
+  console.log(`Action For Button Down Is ${btns.down}`);
+  console.log(`Action For Button Left Is ${btns.left}`);
+}
+
+getActions({
+  up: "Jump",
+  right: "Go Right",
+  down: "Go Down",
+  left: "Go Left",
+});
