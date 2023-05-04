b0903238b4ee4029142c0ca13a4ae77de22a35af
Akir Oussama <akir.oussama@gmail.com>
2023-05-04 07:42:31 +0200

    litéral

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,19 @@
 /* this is a comment */
 console.log(Math.floor(2.3));
+let mouseEvent: "click" | "dblclick" | "mouseup" | "mousedown";
+mouseEvent = "click"; // valid
+mouseEvent = "dblclick"; // valid
+mouseEvent = "mouseup"; // valid
+mouseEvent = "mousedown"; // valid
+mouseEvent = "mouseover"; // compiler error
+
+// on peut utiliser les alias
+// type MouseEvent: 'click' | 'dblclick' | 'mouseup' | 'mousedown';
+// let mouseEvent: MouseEvent;
+// mouseEvent = 'click'; // valid
+// mouseEvent = 'dblclick'; // valid
+// mouseEvent = 'mouseup'; // valid
+// mouseEvent = 'mousedown'; // valid
+// mouseEvent = 'mouseover'; // compiler error
+
+// let anotherEvent: MouseEvent;
