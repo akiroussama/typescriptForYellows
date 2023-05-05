131d7d392496eda31603eb0a5436eda6685081a1
Akir Oussama <akir.oussama@gmail.com>
2023-05-05 08:15:54 +0200

    intro interface

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,35 @@
-/* this is a comment */
-console.log(Math.floor(2.3));
+/*
+  Interface
+  - Interface Declaration
+  --- Serve Like Types
+  --- The Interface Describes The Shape Of An Object
+  --- It Defines The Syntax To Follow
+
+  --- Use With Object
+  --- Use With Function
+  --- Use Read Only And Optional Operator
+*/
+
+interface User {
+  id?: number;
+  readonly username: string;
+  country: string;
+}
+
+let user: User = {
+  id: 100,
+  username: "Francesco",
+  country: "Italy",
+};
+
+user.country = "Singapore";
+
+console.log(user);
+
+function getData(data: User) {
+  console.log(`Id Is ${data.id}`);
+  console.log(`Username Is ${data.username}`);
+  console.log(`Country Is ${data.country}`);
+}
+
+getData({ id: 200, username: "Quentin", country: "Paraguay" });
