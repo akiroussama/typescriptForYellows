c56ddce59e21ad8fc8ab8d1741cb17751d4bcfd3
Akir Oussama <akir.oussama@gmail.com>
2023-05-05 08:54:36 +0200

    htmlelement

--- src/index.ts
+++ src/index.ts
@@ -3,26 +3,26 @@
   - Extending Interfaces
 */
 
-// interface User {
-//   id: number;
-//   username: string;
-//   country: string;
-// }
+interface User {
+  id: number;
+  username: string;
+  country: string;
+}
 
-// // interface moderateur sans l'extends
-// interface Moderator {
-//   id: number;
-//   username: string;
-//   country: string;
-//   role: string | number;
-// }
+// interface moderateur sans l'extends
+interface Moderator {
+  id: number;
+  username: string;
+  country: string;
+  role: string | number;
+}
 
-// let user: Moderator = {
-//   id: 100,
-//   username: "Augustin",
-//   country: "Pays-Bas",
-//   role: "Modérateur",
-// };
+let user: Moderator = {
+  id: 100,
+  username: "Augustin",
+  country: "Pays-Bas",
+  role: "Modérateur",
+};
 
 // deuxième partie: interface moderateur avec l'extends
 // interface User {
@@ -40,28 +40,30 @@
 // }
 
 // quatrième partie: interface admin avec l'extends et l'override
-interface User {
-  id: number;
-  username: string;
-  country: string;
-}
-interface Moderator {
-  id: number;
-  username: string;
-  country: string;
-  role: string | number;
-}
-interface Admin extends Moderator, User {
-  protect?: boolean;
-  role: string; // boolean
-}
+// interface User {
+//   id: number;
+//   username: string;
+//   country: string;
+// }
+// interface Moderator {
+//   id: number;
+//   username: string;
+//   country: string;
+//   role: string | number;
+// }
+// interface Admin extends Moderator, User {
+//   protect?: boolean;
+//   role: string; // boolean
+// }
 
-let user: Admin = {
-  id: 100,
-  username: "Marine",
-  country: "FR",
-  role: "Super Admin",
-  protect: true,
-};
+// let user: Admin = {
+//   id: 100,
+//   username: "Marine",
+//   country: "FR",
+//   role: "Super Admin",
+//   protect: true,
+// };
 
 console.log(user.id);
+// exemple d'utilisation d'une interface
+let superTypescript = document.getElementById("typescript") as HTMLElement;
