7f751cb445589d781cbb862ae27ba269efd201df
Akir Oussama <akir.oussama@gmail.com>
2023-05-05 08:11:55 +0200

    méthode et params

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,33 @@
-/* this is a comment */
-console.log(Math.floor(2.3));
+/*
+  Interface
+  - Interface Méthode And Paramètres
+*/
+
+interface User {
+  id: number;
+  username: string;
+  country: string;
+  sayHello(): string;
+  sayWelcome: () => string;
+  getDouble(num: number): number;
+}
+
+let user: User = {
+  id: 100,
+  username: "Ludivine",
+  country: "Inde",
+  sayHello() {
+    return `Hello ${this.username}`;
+  },
+  sayWelcome: () => {
+    return `Welcome ${user.username}`;
+  },
+  getDouble(n) {
+    return n * 2;
+  },
+};
+
+console.log(user.id);
+console.log(user.sayHello());
+console.log(user.sayWelcome());
+console.log(user.getDouble(100));
